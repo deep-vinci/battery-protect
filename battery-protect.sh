05eb252e4cad7785aad5fa180f802b4af28f970c
Deepak <deepakworkpc@gmail.com>
2024-10-04 22:03:36 +0530

    feat: make it compatible with crontab, add icon

--- battery-protect.sh
+++ battery-protect.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+export DISPLAY=":1"  
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
+
 batteryPercentage=$(upower -i $(upower -e | grep 'BAT') | grep -E "percentage" | awk '{print $2}' | sed 's/%//')
 batteryStatus=$(upower -i $(upower -e | grep 'BAT') | grep -E "state" | awk '{print $2}' | sed 's/%//')
 upperThresholdPercentage=80
 
-# Path to a file that will control notifications
 CONTROL_FILE="$HOME/battery-enable"
 
 # Check if notifications are enabled
@@ -12,11 +14,9 @@ if [ ! -f "$CONTROL_FILE" ]; then
     exit 0
 fi
 
-
 if [ "$batteryStatus" == "charging" ]; then
-
     if [ $batteryPercentage -ge $upperThresholdPercentage ]; then
-        notify-send "Remove Charger" "Battery $batteryPercentage% Charged" --app-name="Battery Protect"
+        echo "$(date)" > log.txt
+        /usr/bin/notify-send "Remove Charger" "Battery $batteryPercentage% Charged." --icon="$HOME/Documents/stupid-scripts/battery-protect/danger-logo.png"  --urgency="critical" --app-name="Battery Protect"
     fi
-
 fi
