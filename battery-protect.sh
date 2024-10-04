755235b037b6471a95b73940de43fa2968073449
Deepak <deepakworkpc@gmail.com>
2024-10-04 15:56:19 +0530

    feat: option to disable the notification

--- battery-protect.sh
+++ battery-protect.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 batteryPercentage=$(upower -i $(upower -e | grep 'BAT') | grep -E "percentage" | awk '{print $2}' | sed 's/%//')
+batteryStatus=$(upower -i $(upower -e | grep 'BAT') | grep -E "state" | awk '{print $2}' | sed 's/%//')
 upperThresholdPercentage=20
 
-if [ $batteryPercentage -ge $upperThresholdPercentage ]; then
-    notify-send "Remove Charger" "Battery $batteryPercentage% Charged" --app-name="Battery Protect" --expire-time=5000
+# Path to a file that will control notifications
+CONTROL_FILE="$HOME/battery-enable"
+
+# Check if notifications are enabled
+if [ ! -f "$CONTROL_FILE" ]; then
+    exit 0
+fi
+
+
+if [ "$batteryStatus" == "charging" ]; then
+
+    if [ $batteryPercentage -ge $upperThresholdPercentage ]; then
+        notify-send "Remove Charger" "Battery $batteryPercentage% Charged" --app-name="Battery Protect" --expire-time=5000
+    fi
+
 fi
